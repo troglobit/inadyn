39b25e30a8be5e86d4eaa111b9d8da3fd6e850ee
Joachim Nilsson <troglobit@vmlinux.org>
2010-10-14 08:51:13 +0200

    Refactor and cleanup Makefile

---
+++ config.mk
@@ -0,0 +1,27 @@
+# Make command to use for dependencies
+MAKECMD	      = make
+
+INADYN_BASE   = .
+SRCDIR	      = $(INADYN_BASE)/src
+OUTDIR	      = bin
+
+# Oversimplified arch setup, no smart detection.
+# Possible values: linux, mac, solaris, yourown
+ifndef TARGET_ARCH
+TARGET_ARCH   = linux
+endif
+
+ifeq ($(TARGET_ARCH),solaris)
+EXTRA_CFLAGS  =
+EXTRA_OBJS    =
+EXTRA_LIBS    = -lsocket -lnsl
+endif
+ifeq ($(TARGET_ARCH),linux)
+EXTRA_CFLAGS  =
+EXTRA_OBJS    =
+EXTRA_LIBS    =
+endif
+
+ifdef DEBUG
+EXTRA_CFLAGS += -g
+endif
