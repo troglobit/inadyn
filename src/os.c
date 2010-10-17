d8b81f34c66e79dcfee54bfbc295343ccd4fe887
Joachim Nilsson <troglobit@vmlinux.org>
2010-10-17 23:01:09 +0200

    Merge with inadyn-advanced: Add support for datetime in debug messages.

--- src/os.c
+++ src/os.c
@@ -1,25 +1,32 @@
 /*
-Copyright (C) 2003-2004 Narcis Ilisei
+ * Copyright (C) 2003-2004  Narcis Ilisei
+ * Copyright (C) 2006  Steve Horbachuk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
-  
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
+#include <time.h>
+
 #include "os.h"
 #include "debug_if.h"
+
+#define MAXSTRING 1024
+
 /*
     returns the inet addr of a char* begining with an IP address: "nnn.nnn.nnn.nnn"
 */
@@ -29,35 +36,35 @@ RC_TYPE os_convert_ip_to_inet_addr(unsigned int *p_addr, const char *p_name)
 
     if (p_name == NULL || p_addr == NULL)
     {
-        return RC_INVALID_POINTER;
+	return RC_INVALID_POINTER;
     }
 
     *p_addr = 0;
     if (strlen(p_name) == 0)
     {
-        rc = RC_OS_INVALID_IP_ADDRESS;
+	rc = RC_OS_INVALID_IP_ADDRESS;
     }
 
     {
-        unsigned int b3;
-        unsigned int b2;
-        unsigned int b1;
-        unsigned int b0;
-        unsigned int ipa;
-        int n;
+	unsigned int b3;
+	unsigned int b2;
+	unsigned int b1;
+	unsigned int b0;
+	unsigned int ipa;
+	int n;
 
-        ipa = 0x0;
-        n = sscanf(p_name, IP_V4_IP_ADDR_FORMAT, &b3, &b2, &b1, &b0);
-        if (n == 4)
-        {
-            ipa = (b3 << 24) | (b2 << 16) | (b1 << 8) | b0;
-            *p_addr = htonl(ipa);
-            
-        }
-        else
-        {
-            rc = RC_OS_INVALID_IP_ADDRESS;
-        }
+	ipa = 0x0;
+	n = sscanf(p_name, IP_V4_IP_ADDR_FORMAT, &b3, &b2, &b1, &b0);
+	if (n == 4)
+	{
+	    ipa = (b3 << 24) | (b2 << 16) | (b1 << 8) | b0;
+	    *p_addr = htonl(ipa);
+
+	}
+	else
+	{
+	    rc = RC_OS_INVALID_IP_ADDRESS;
+	}
     }
     return rc;
 }
@@ -79,13 +86,34 @@ DBG_DEST get_dbg_dest(void)
 }
 
 void set_dbg_dest(DBG_DEST dest)
-{    
+{
     global_mod_dbg_dest = dest;
 }
 
-#include <stdarg.h> 
+char *print_time(void)
+{
+    time_t now;
+    struct tm *timeptr;
+    static const char wday_name[7][3] = {
+	"Sun", "Mon", "Tue", "Wed",
+	"Thu", "Fri", "Sat"
+    };
+    static const char mon_name[12][3] = {
+	"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+	"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+    static char result[26];
 
-#define MAXSTRING 1024
+    time(&now);
+    timeptr = localtime(&now);
+
+    sprintf(result, "%.3s %.3s%3d %.2d:%.2d:%.2d %d:",
+	wday_name[timeptr->tm_wday], mon_name[timeptr->tm_mon],
+	timeptr->tm_mday, timeptr->tm_hour, timeptr->tm_min,
+	timeptr->tm_sec, 1900 + timeptr->tm_year);
+
+    return result;
+}
 
 void os_printf(int prio, char *fmt, ... )
 {
@@ -100,12 +128,12 @@ void os_printf(int prio, char *fmt, ... )
 #ifdef HAVE_OS_SYSLOG
     if (get_dbg_dest() == DBG_SYS_LOG)
     {
-        syslog(prio, "%s",message);
+	syslog(prio, "%s",message);
     }
     else
 #endif
     {
-        printf("%s",message); fflush(stdout);
+	printf("%s %s", print_time(), message); fflush(stdout);
     }
 
     return;
@@ -115,10 +143,10 @@ void os_printf(int prio, char *fmt, ... )
 
 /**
  * Opens the dbg output for the required destination.
- * 
+ *
  * WARNING : Open and Close bg output are quite error prone!
  * They should be called din pairs!
- * TODO: 
+ * TODO:
  *  some simple solution that involves storing the dbg output device name (and filename)
  */
 RC_TYPE os_open_dbg_output(DBG_DEST dest, const char *p_prg_name, const char *p_logfile_name)
@@ -129,31 +157,31 @@ RC_TYPE os_open_dbg_output(DBG_DEST dest, const char *p_prg_name, const char *p_
     switch (get_dbg_dest())
     {
     case DBG_SYS_LOG:
-        if (p_prg_name == NULL)
-        {
-            rc = RC_INVALID_POINTER;
-            break;
-        }
-        rc = os_syslog_open(p_prg_name);
-        break;
+	if (p_prg_name == NULL)
+	{
+	    rc = RC_INVALID_POINTER;
+	    break;
+	}
+	rc = os_syslog_open(p_prg_name);
+	break;
 
     case DBG_FILE_LOG:
-        if (p_logfile_name == NULL)
-        {
-            rc = RC_INVALID_POINTER;
-            break;
-        }
-        {        
-            FILE *pF = freopen(p_logfile_name, "wb", stdout);
-            if (pF == NULL)
-            {
-                rc = RC_FILE_IO_OPEN_ERROR;
-            }
-            break;
-        }
+	if (p_logfile_name == NULL)
+	{
+	    rc = RC_INVALID_POINTER;
+	    break;
+	}
+	{
+	    FILE *pF = freopen(p_logfile_name, "wb", stdout);
+	    if (pF == NULL)
+	    {
+		rc = RC_FILE_IO_OPEN_ERROR;
+	    }
+	    break;
+	}
     case DBG_STD_LOG:
     default:
-        rc = RC_OK;
+	rc = RC_OK;
     }
     return rc;
 }
@@ -167,19 +195,17 @@ RC_TYPE os_close_dbg_output(void)
     switch (get_dbg_dest())
     {
     case DBG_SYS_LOG:
-        rc = os_syslog_close();
-        break;
+	rc = os_syslog_close();
+	break;
 
     case DBG_FILE_LOG:
-        fclose(stdout);
-        rc = RC_OK;
-        break;
+	fclose(stdout);
+	rc = RC_OK;
+	break;
 
     case DBG_STD_LOG:
-    default:        
-        rc = RC_OK;
+    default:
+	rc = RC_OK;
     }
     return rc;
 }
-
-
