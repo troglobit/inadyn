446c4ac692ef9ad6bffe4730ca9929ac917a0b96
Joachim Nilsson <troglobit@gmail.com>
2015-10-09 07:38:25 +0200

    Fix issue #110: Poodle SSL_MODE_SEND_FALLBACK_SCSV not needed.
    The POODLE fixes that introduced SSL_MODE_SEND_FALLBACK_SCSV are
    not needed when the client does not perform protocol fallbacks.
    
    For more on this, and OpenSSL in general, see
    
        https://wiki.openssl.org/index.php/SSL_MODE_SEND_FALLBACK_SCSV
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/ssl.c
+++ src/ssl.c
@@ -1,6 +1,6 @@
 /* Interface for optional HTTPS functions
  *
- * Copyright (C) 2014  Joachim Nilsson <troglobit@gmail.com>
+ * Copyright (C) 2014-2015  Joachim Nilsson <troglobit@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -63,7 +63,7 @@ int ssl_init(http_t *client, char *msg)
 
 #if defined(CONFIG_OPENSSL)
 	/* POODLE, only allow TLSv1.x or later */
-	SSL_CTX_set_options(client->ssl_ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
+	SSL_CTX_set_options(client->ssl_ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION);
 #else
 	/* GnuTLS already defaults to TLS 1.x */
 #endif
@@ -71,10 +71,6 @@ int ssl_init(http_t *client, char *msg)
 	client->ssl = SSL_new(client->ssl_ctx);
 	if (!client->ssl)
 		return RC_HTTPS_OUT_OF_MEMORY;
-
-#ifdef SSL_MODE_SEND_FALLBACK_SCSV
-	SSL_set_mode(client->ssl, SSL_MODE_SEND_FALLBACK_SCSV);
-#endif
 
 	http_get_remote_name(client, &sn);
 	if (set_server_name(client->ssl, sn))
