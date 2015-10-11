e3e7c42c2add8bbf7b5620d92111c00f1a7ef0ab
Joachim Nilsson <troglobit@gmail.com>
2015-10-11 21:43:36 +0200

    Upgrade libite (LITE) to v1.1.1
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- plugins/dhis.c
+++ plugins/dhis.c
@@ -60,7 +60,7 @@ static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 			info->server_name.name);
 }
 
-static int response(http_trans_t *trans, ddns_info_t *UNUSED(info), ddns_alias_t *UNUSED(alias))
+static int response(http_trans_t *trans, ddns_info_t *UNUSED(info), ddns_alias_t *alias)
 {
 	char *rsp = trans->p_rsp_body;
 
