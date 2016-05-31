troglobit/inadyn

commit 5a679d9fdb84b21e417af2aad46f1f24128e3a7b

Change inadyn.conf 'alias' setting to 'hostname'
This patch deprecates the 'alias' setting in favor of 'hostname'.  Same
behavior, only a different wrapping.

A validator callback is added to give a smooth transition to the new
syntax for users.  A deprecated warning will be printed though, urging
the user to update the config file.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/conf.c
@@ -44,6 +44,33 @@
  */
 static LIST_HEAD(head, di) info_list = LIST_HEAD_INITIALIZER(info_list);
 
+/*
+ * Convert deprecated 'alias' setting to new 'hostname',
+ * same functionality with new name.
+ */
+static int deprecate_alias(cfg_t *cfg)
+{
+	size_t i;
+	cfg_opt_t *alias, *hostname;
+
+	alias = cfg_getopt(cfg, "alias");
+	if (!alias || cfg_opt_size(alias) <= 0)
+		return 0;
+
+	hostname = cfg_getopt(cfg, "hostname");
+	if (cfg_opt_size(hostname) > 0) {
+		cfg_error(cfg, "Both 'hostname' and 'alias' set, cannot convert deprecated 'alias' to 'hostname'");
+		return -1;
+	}
+
+	cfg_error(cfg, "converting 'alias' to 'hostname'.");
+	for (i = 0; i < cfg_opt_size(alias); i++)
+		cfg_opt_setnstr(hostname, cfg_opt_getnstr(alias, i), i);
+
+	cfg_free_value(alias);
+
+	return 0;
+}
 
 static int validate_period(cfg_t *cfg, cfg_opt_t *opt)
 {
@@ -57,26 +84,26 @@ static int validate_period(cfg_t *cfg, cfg_opt_t *opt)
 	return 0;
 }
 
-static int validate_alias(cfg_t *cfg, const char *provider, cfg_opt_t *alias)
+static int validate_hostname(cfg_t *cfg, const char *provider, cfg_opt_t *hostname)
 {
 	size_t i;
 
-	if (!alias) {
-		cfg_error(cfg, "Missing DDNS alias setting in provider %s", provider);
+	if (!hostname) {
+		cfg_error(cfg, "DDNS hostname setting is missing in provider %s", provider);
 		return -1;
 	}
 
-	if (!cfg_opt_size(alias)) {
-		cfg_error(cfg, "No aliases listed in DDNS provider %s", provider);
+	if (!cfg_opt_size(hostname)) {
+		cfg_error(cfg, "No hostnames listed in DDNS provider %s", provider);
 		return -1;
 	}
 
-	for (i = 0; i < cfg_opt_size(alias); i++) {
-		char *name = cfg_opt_getnstr(alias, i);
+	for (i = 0; i < cfg_opt_size(hostname); i++) {
+		char *name = cfg_opt_getnstr(hostname, i);
 		ddns_info_t info;
 
 		if (sizeof(info.alias[0].name) < strlen(name)) {
-			cfg_error(cfg, "Too long DDNS alias (%s) in provider %s", name, provider);
+			cfg_error(cfg, "Too long DDNS hostname (%s) in provider %s", name, provider);
 			return -1;
 		}
 	}
@@ -102,7 +129,8 @@ static int validate_common(cfg_t *cfg, const char *provider, int custom)
 		return -1;
 	}
 
-	return validate_alias(cfg, provider, cfg_getopt(cfg, "alias"));
+	return deprecate_alias(cfg) ||
+		validate_hostname(cfg, provider, cfg_getopt(cfg, "hostname"));
 }
 
 static int validate_provider(cfg_t *cfg, cfg_opt_t *opt)
@@ -213,10 +241,10 @@ static int set_provider_opts(cfg_t *cfg, ddns_info_t *info, int custom)
 	if (str && strlen(str) <= sizeof(info->creds.password))
 		strlcpy(info->creds.password, str, sizeof(info->creds.password));
 
-	for (j = 0; j < cfg_size(cfg, "alias"); j++) {
+	for (j = 0; j < cfg_size(cfg, "hostname"); j++) {
 		size_t pos = info->alias_count;
 
-		str = cfg_getnstr(cfg, "alias", j);
+		str = cfg_getnstr(cfg, "hostname", j);
 		if (!str)
 			continue;
 
@@ -327,7 +355,8 @@ cfg_t *conf_parse_file(char *file, ddns_t *ctx)
 	cfg_opt_t provider_opts[] = {
 		CFG_STR     ("username",     NULL, CFGF_NONE),
 		CFG_STR     ("password",     NULL, CFGF_NONE),
-		CFG_STR_LIST("alias",        NULL, CFGF_NONE),
+		CFG_STR_LIST("hostname",     NULL, CFGF_NONE),
+		CFG_STR_LIST("alias",        NULL, CFGF_DEPRECATED),
 		CFG_BOOL    ("ssl",          cfg_false, CFGF_NONE),
 		CFG_BOOL    ("wildcard",     cfg_false, CFGF_NONE),
 		CFG_END()
@@ -336,7 +365,8 @@ cfg_t *conf_parse_file(char *file, ddns_t *ctx)
 		/* Same as a general provider */
 		CFG_STR     ("username",     NULL, CFGF_NONE),
 		CFG_STR     ("password",     NULL, CFGF_NONE),
-		CFG_STR_LIST("alias",        NULL, CFGF_NONE),
+		CFG_STR_LIST("hostname",     NULL, CFGF_NONE),
+		CFG_STR_LIST("alias",        NULL, CFGF_DEPRECATED),
 		CFG_BOOL    ("ssl",          cfg_false, CFGF_NONE),
 		CFG_BOOL    ("wildcard",     cfg_false, CFGF_NONE),
 		/* Custom settings */
@@ -361,7 +391,7 @@ cfg_t *conf_parse_file(char *file, ddns_t *ctx)
 	};
 	cfg_t *cfg;
 
-	cfg = cfg_init(opts, CFGF_NONE);
+	cfg = cfg_init(opts, CFGF_IGNORE_UNKNOWN);
 	if (!cfg) {
 		logit(LOG_ERR, "Failed initializing configuration file parser: %m");
 		return NULL;

man/inadyn.conf.5
@@ -107,8 +107,8 @@ Use HTTPS to connect to this DDNS service provider, default HTTP (false).
 The username, if applicable.  This might be referred to as hash by some providers.
 .It Cm password = PASSWORD
 The password, if applicable.
-.It Cm alias = ALIAS
-.It Cm alias = { "ALIAS1.name.tld", "ALIAS2.name.tld" }
+.It Cm hostname = HOSTNAME
+.It Cm hostname = { "HOSTNAME1.name.tld", "HOSTNAME2.name.tld" }
 Your hostname alias.  To list multiple names, use the second form.
 .It Cm wildcard = true
 Enable domain name wildcarding of your domain name, for DDNS providers
@@ -174,12 +174,12 @@ Specific to the custom provider section are the following settings:
 .It Cm ddns-server = update.example.com
 DDNS server name, not the full URL.
 .It Cm ddns-path   = "/update?domain="
-DDNS server path.  By default each listed alias is appended to the path, unless
+DDNS server path.  By default the hostname is appended to the path,
+unless
 .Cm append-myip=true
 is set.
 .It Cm append-myip = true
-Append your current IP to the the DDNS server update path.  Default
-is 'false', which means each alias name is appended.
+Append your current IP to the the DDNS server update path.
 .It Cm checkip-server = checkip.example.com
 Server used to check for IP address updates.
 .It Cm checkip-path = "/"
@@ -203,7 +203,7 @@ provider default@dyndns.org {
 .br
     password   = secret1
 .br
-    alias      = { "my.example.com", "other.example.org" }
+    hostname   = { "my.example.com", "other.example.org" }
 .br
 }
 .Pp
@@ -215,7 +215,7 @@ provider default@no-ip.com:1 {
 .br
     password   = secret21
 .br
-    alias      = example.no-ip.com
+    hostname   = example.no-ip.com
 .br
 }
 .Pp
@@ -227,7 +227,7 @@ provider default@no-ip.com:2 {
 .br
     password   = secret22
 .br
-    alias      = another.no-ip.com
+    hostname   = another.no-ip.com
 .br
 }
 .Pp
@@ -243,7 +243,7 @@ provider default@loopia.com {
 .br
     password   = secret3
 .br
-    alias      = example.com
+    hostname   = example.com
 .br
 }
 .Pp
@@ -257,7 +257,7 @@ provider default@ddnss.de {
 .br
     password   = your_password
 .br
-    alias      = your_host.ddnss.de
+    hostname   = your_host.ddnss.de
 .br
 }
 .Pp
@@ -269,7 +269,7 @@ provider default@dynv6.com {
 .br
     password   = not_used
 .br
-    alias      = your_host.dynv6.net
+    hostname   = your_host.dynv6.net
 .br
 }
 .Pp
@@ -283,7 +283,7 @@ provider default@tunnelbroker.net {
 .br
     password   = update-key-in-advanced-tab
 .br
-    alias      = tunnel-id
+    hostname   = tunnel-id
 .br
 }
 .Pp
@@ -304,7 +304,7 @@ custom twoDNS.de {
 .br
     ddns-path      = "/update?hostname="
 .br
-    alias          = example.dd-dns.de
+    hostname       = example.dd-dns.de
 .br
 }
 .Pp
@@ -329,14 +329,14 @@ custom dyn.com {
 .br
     append-myip  = true
 .br
-    alias        = YOURHOST
+    hostname     = YOURHOST
 .br
 }
 .Pp
 Notice the use of
 .Nm append-myip
 which differs from above previous examples.  Without this option set the
-default (backwards compatible) behavior is to add the hostname alias.
+default (backwards compatible) behavior is to add the hostname.
 .Sh "SEE ALSO"
 .Xr inadyn 8
 .Pp

commit af64d0bf89236a53d3801cbf4a061499d2ed7a42

Do not ignore unknown .conf settings
This patch fixes a regression introduced in 5a679d9 where `cfg_init()`
began ignoring unknown options, causing the parser to simply return a
silly "parse error" with no pointer to the user what was wrong.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/conf.c
@@ -421,7 +421,7 @@ cfg_t *conf_parse_file(char *file, ddns_t *ctx)
 	};
 	cfg_t *cfg;
 
-	cfg = cfg_init(opts, CFGF_IGNORE_UNKNOWN);
+	cfg = cfg_init(opts, CFGF_NONE);
 	if (!cfg) {
 		logit(LOG_ERR, "Failed initializing configuration file parser: %m");
 		return NULL;