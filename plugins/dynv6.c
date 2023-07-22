troglobit/inadyn

commit fba6267fbeb5a1b4eccc1ba7c41e0f658fa68ec0

Refactor, register update request string with plugin
 - Simplifies many plugins with similar code paths for ipv4 and ipv6
 - Allows for improved debugging and diagnostics

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

plugins/dynv6.c
@@ -24,10 +24,18 @@
 
 #define DYNV6_UPDATE_IP_REQUEST						\
 	"GET %s?"							\
-	"%s=%s&"							\
+	"ipv4=%s&"							\
 	"hostname=%s&"							\
 	"token=%s"							\
-	" "								\
+	"HTTP/1.0\r\n"							\
+	"Host: %s\r\n"							\
+	"User-Agent: %s\r\n\r\n"
+
+#define DYNV6_UPDATE_IP6_REQUEST					\
+	"GET %s?"							\
+	"ipv6=%s&"							\
+	"hostname=%s&"							\
+	"token=%s "							\
 	"HTTP/1.0\r\n"							\
 	"Host: %s\r\n"							\
 	"User-Agent: %s\r\n\r\n"
@@ -65,20 +73,17 @@ static ddns_system_t plugin6 = {
 
 static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 {
-	const char *ver = "ipv6";
 	char *addr;
 
-	if (strstr(info->system->name, "ipv4"))
-		ver = "ipv4";
 	if (alias->address[0])
 		addr = alias->address;
 	else
 		addr = "auto";	/* ipv6=auto, or ipv4=auto */
 
 	return snprintf(ctx->request_buf, ctx->request_buflen,
-			DYNV6_UPDATE_IP_REQUEST,
+			info->system->server_req,
 			info->server_url,
-			ver, addr,
+			addr,
 			alias->name,
 			info->creds.username,
 			info->server_name.name,
@@ -102,8 +107,8 @@ static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *alias)
 
 PLUGIN_INIT(plugin_init)
 {
-	plugin_register(&plugin4);
-	plugin_register(&plugin6);
+	plugin_register(&plugin4, DYNV6_UPDATE_IP_REQUEST);
+	plugin_register(&plugin6, DYNV6_UPDATE_IP6_REQUEST);
 }
 
 PLUGIN_EXIT(plugin_exit)

include/plugin.h
@@ -24,6 +24,7 @@
 #ifndef INADYN_PLUGIN_H_
 #define INADYN_PLUGIN_H_
 
+#include "config.h"
 #include "queue.h"		/* BSD sys/queue.h API */
 
 #define GENERIC_HTTP_REQUEST                                      	\
@@ -45,7 +46,9 @@ typedef int (*rsp_fn_t) (void *this, void *info, void *alias);
 typedef struct ddns_system {
 	TAILQ_ENTRY(ddns_system) link; /* BSD sys/queue.h linked list node. */
 
-	char    *name;
+	char          *name;
+
+	int            cloned;	      /* marks this plugin as a clone */
 
 	setup_fn_t     setup;
 	req_fn_t       request;
@@ -59,13 +62,13 @@ typedef struct ddns_system {
 
 	const char    *server_name;
 	const char    *server_url;
-	int            cloned; /* marks this plugin as a clone */
+	const char    *server_req;
 } ddns_system_t;
 
 /* Public plugin API */
-int plugin_register   (ddns_system_t *system);
-int plugin_register_v6   (ddns_system_t *system);
-int plugin_unregister (ddns_system_t *system);
+int            plugin_register    (ddns_system_t *system, const char *req);
+int            plugin_register_v6 (ddns_system_t *system, const char *req);
+int            plugin_unregister  (ddns_system_t *system);
 
 /* Helper API */
 ddns_system_t *plugin_find (const char *name, int loose);

plugins/all-inkl.c
@@ -21,7 +21,7 @@
 
 #include "plugin.h"
 
-#define ALL_INKL_UPDATE_IP_REQUEST						\
+#define ALL_INKL_UPDATE_IP_REQUEST					\
 	"GET %s?"							\
 	"myip=%s "							\
 	"HTTP/1.0\r\n"							\
@@ -50,7 +50,7 @@ static ddns_system_t plugin = {
 static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 {
 	return snprintf(ctx->request_buf, ctx->request_buflen,
-			ALL_INKL_UPDATE_IP_REQUEST,
+			info->system->server_req,
 			info->server_url,
 			alias->address,
 			info->server_name.name,
@@ -70,8 +70,8 @@ static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *alias)
 
 PLUGIN_INIT(plugin_init)
 {
-	plugin_register(&plugin);
-	plugin_register_v6(&plugin);
+	plugin_register(&plugin, ALL_INKL_UPDATE_IP_REQUEST);
+	plugin_register_v6(&plugin, ALL_INKL_UPDATE_IP_REQUEST);
 }
 
 PLUGIN_EXIT(plugin_exit)

plugins/changeip.c
@@ -81,7 +81,7 @@ static ddns_system_t strato = {
 static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 {
 	return snprintf(ctx->request_buf, ctx->request_buflen,
-			CHANGEIP_UPDATE_IP_HTTP_REQUEST,
+			info->system->server_req,
 			info->server_url,
 			alias->name,
 			alias->address,
@@ -97,11 +97,11 @@ static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *alias)
 
 PLUGIN_INIT(plugin_init)
 {
-	plugin_register(&plugin);
-	plugin_register(&ovh);
-	plugin_register_v6(&ovh);
-	plugin_register(&strato);
-	plugin_register_v6(&strato);
+	plugin_register(&plugin, CHANGEIP_UPDATE_IP_HTTP_REQUEST);
+	plugin_register(&ovh, CHANGEIP_UPDATE_IP_HTTP_REQUEST);
+	plugin_register_v6(&ovh, CHANGEIP_UPDATE_IP_HTTP_REQUEST);
+	plugin_register(&strato, CHANGEIP_UPDATE_IP_HTTP_REQUEST);
+	plugin_register_v6(&strato, CHANGEIP_UPDATE_IP_HTTP_REQUEST);
 }
 
 PLUGIN_EXIT(plugin_exit)

plugins/cloudflare.c
@@ -378,7 +378,7 @@ static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *hostname)
 			content_len, json_data);
 
 	return snprintf(ctx->request_buf, ctx->request_buflen,
-			CLOUDFLARE_HOSTNAME_UPDATE_REQUEST,
+			info->system->server_req,
 			data->zone_id,
 			data->hostname_id,
 			info->user_agent,
@@ -402,8 +402,8 @@ static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *hostna
 
 PLUGIN_INIT(plugin_init)
 {
-	plugin_register(&plugin);
-	plugin_register_v6(&plugin);
+	plugin_register(&plugin, CLOUDFLARE_HOSTNAME_UPDATE_REQUEST);
+	plugin_register_v6(&plugin, CLOUDFLARE_HOSTNAME_UPDATE_REQUEST);
 }
 
 PLUGIN_EXIT(plugin_exit)

plugins/cloudxns.c
@@ -338,7 +338,7 @@ static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 		return -1;
 
 	return snprintf(ctx->request_buf, ctx->request_buflen,
-			CLOUDXNS_UPDATE_IP_REQUEST,
+			info->system->server_req,
 			info->server_url,
 			cx->record_id,
 			info->server_name.name,
@@ -374,7 +374,7 @@ static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *alias)
 
 PLUGIN_INIT(plugin_init)
 {
-	plugin_register(&plugin);
+	plugin_register(&plugin, CLOUDXNS_UPDATE_IP_REQUEST);
 }
 
 PLUGIN_EXIT(plugin_exit)

plugins/common.c
@@ -24,22 +24,6 @@
 #include "plugin.h"
 
 /*
- * dyndns.org specific update address format
- *
- * Also applies to other dyndns2 api compatible services, like:
- * DNS-O-Matic, no-ip, 3322, HE and nsupdate.info.
- */
-#define DYNDNS_UPDATE_IP_HTTP_REQUEST					\
-	"GET %s?"							\
-	"hostname=%s&"							\
-	"myip=%s"							\
-	"%s "      							\
-	"HTTP/1.0\r\n"							\
-	"Host: %s\r\n"							\
-	"Authorization: Basic %s\r\n"					\
-	"User-Agent: %s\r\n\r\n"
-
-/*
  * DynDNS request composer -- common to many other DDNS providers as well
  */
 int common_request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
@@ -50,7 +34,7 @@ int common_request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 		strlcpy(wildcard, "&wildcard=ON", sizeof(wildcard));
 
 	return snprintf(ctx->request_buf, ctx->request_buflen,
-			DYNDNS_UPDATE_IP_HTTP_REQUEST,
+			info->system->server_req,
 			info->server_url,
 			alias->name,
 			alias->address,

plugins/core-networks.c
@@ -21,7 +21,7 @@
 
 #include "plugin.h"
 
-#define CORE_NETWORKS_UPDATE_IP_REQUEST						\
+#define CORE_NETWORKS_UPDATE_IP_REQUEST					\
 	"GET %s?"							\
 	"hostname=%s&"							\
 	"myip=%s&"							\
@@ -58,7 +58,7 @@ static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 		keepip = "keepipv6=1";
 
 	return snprintf(ctx->request_buf, ctx->request_buflen,
-			CORE_NETWORKS_UPDATE_IP_REQUEST,
+			info->system->server_req,
 			info->server_url,
 			alias->name,
 			alias->address,
@@ -85,8 +85,8 @@ static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *alias)
 
 PLUGIN_INIT(plugin_init)
 {
-	plugin_register(&plugin);
-	plugin_register_v6(&plugin);
+	plugin_register(&plugin, CORE_NETWORKS_UPDATE_IP_REQUEST);
+	plugin_register_v6(&plugin, CORE_NETWORKS_UPDATE_IP_REQUEST);
 }
 
 PLUGIN_EXIT(plugin_exit)

commit cffc7aff47756c018331bb7b072970d24ee4ae1b

Fix #433: regression introduced in fba6267f (V2.11.0)
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

plugins/dynv6.c
@@ -26,7 +26,7 @@
 	"GET %s?"							\
 	"ipv4=%s&"							\
 	"hostname=%s&"							\
-	"token=%s"							\
+	"token=%s "							\
 	"HTTP/1.0\r\n"							\
 	"Host: %s\r\n"							\
 	"User-Agent: %s\r\n\r\n"