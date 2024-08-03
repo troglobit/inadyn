0e2ca31ba8454319cf57bcd8f7bc972f5fa6f4d0
Joachim Wiberg <troglobit@gmail.com>
2024-08-03 11:56:54 +0200

    plugins: whitespace only
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- plugins/porkbun.c
+++ plugins/porkbun.c
@@ -188,7 +188,7 @@ static int get_result_value(const char *json, const char *key, jsmntok_t *out_re
 	return -1;
 }
 
-	static int json_copy_value(char *dest, size_t dest_size, const char *json, const jsmntok_t *token)
+static int json_copy_value(char *dest, size_t dest_size, const char *json, const jsmntok_t *token)
 {
 	size_t length;
 
